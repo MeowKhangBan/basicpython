da0bbfb1527b75fdb99b02f664c775ef3e02b6c6
MeowKhangBan <103108684+MeowKhangBan@users.noreply.github.com>
2022-04-07 17:32:16 +0700

    Add files via upload

---
+++ EP.2.py
@@ -0,0 +1,29 @@
+from tkinter import W
+import turtle
+omg = turtle.Pen()
+omg.shape('turtle')
+w = 1
+
+def re(w='left',n=1):
+    if w == 'left':
+        for i in range(n):
+            omg.forward(50)
+            omg.left(37)
+    elif w == 'right':
+        for i in range(n):
+            omg.forward(40)
+            omg.right(35)
+
+def go(x,y):
+    omg.penup()
+    omg.goto(x,y)
+    omg.pendown
+
+re('left',50)
+re('right',50)
+re('right',25)
+re('left',50)
+
+
+
+go(200,500)
